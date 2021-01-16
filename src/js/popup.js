7137b2ddfdffc3b20de1c5dfc6b833e1d735f6fd
David Carpenter <carpenterd777@gmail.com>
2021-01-16 18:55:49 -0500

    Added suggestion popup component

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,10 @@
 import "../css/popup.css";
 import React from "react";
 import CartItem from "./popup/CartItem/CartItem.jsx";
+import SuggestionPopup from "./popup/SuggestionPopup/SuggestionPopup.jsx";
 import { render } from "react-dom";
 
 render(
-  <CartItem/>,
+  <SuggestionPopup/>,
   window.document.getElementById("app-container")
 );
