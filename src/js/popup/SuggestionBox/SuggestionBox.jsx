a24fc8264aa6e48b6524312872085d79e5d8aed1
David Carpenter <carpenterd777@gmail.com>
2021-01-17 11:26:33 -0500

    Green suggestion box is here

--- src/js/popup/SuggestionBox/SuggestionBox.jsx
+++ src/js/popup/SuggestionBox/SuggestionBox.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
+import "./SuggestionBox.css"
+
 class SuggestionBox extends React.Component {
 
     constructor() {
         super();
-        this.itemTitle = "Farm-Raised Atlantic Salmon Fillets";
+        this.itemTitle = "Farm-Raised Atlantic Salmon Fillets (32 oz)";
         this.ratingDifference = "x";
         this.itemImg = require("../../../img/salmon2.jpg");
     }
 
     render() {
         return (
-            <div class="suggeestion-box-container">
-                <div class="left">
-                    <div class="item-title">{this.itemTitle}</div>
-                    <div>This product is {this.ratingDifference}% better</div>
+            <div className="suggestion-box-container">
+                <div className="left">
+                    <div className="itemTitle">{this.itemTitle}</div>
+                    <div id="rating-diff">This product is {this.ratingDifference}% better</div>
                 </div>
-            <img src={this.itemImg} alt={`This is a picture of ${this.itemTitle}.`}></img>
+                <img src={this.itemImg} alt={`This is a picture of ${this.itemTitle}.`}></img>
             </div>
-        );
+        )
     }
 }
 
-export default hot(module)(CartItem)
+export default hot(module)(SuggestionBox)
