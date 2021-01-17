befe17c170701cf35522be1f35defee13cd7152a
Bonnie Chen <59186757+chenbonnie@users.noreply.github.com>
2021-01-17 11:40:43 -0500

    suggestion popup fix!

--- src/js/popup/SuggestionPopup/SuggestionPopup.jsx
+++ src/js/popup/SuggestionPopup/SuggestionPopup.jsx
@@ -10,6 +10,7 @@ class SuggestionPopup extends React.Component {
                 <CartItem/>
                 <div class="message">We've found some more sustainable options for you!</div>
                 {/*Insert Bonnie's suggestion object here*/}
+                <div class="popupmessage">Farm-Raised Atlantic Salmon Filets (32 oz)</div>
                 <a href="https://hanahlev.github.io/Hack-Northeast-Sustainablility/">Learn more</a>
             </div>
         )
